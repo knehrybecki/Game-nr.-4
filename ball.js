f9c63c1a259d566bfa305d6085cc77ea5e66a48c
Kamil Nehrybecki <knehrybecki@gmail.com>
2022-04-07 00:31:36 +0200

    Game

---
+++ ball.js
@@ -0,0 +1,56 @@
+import { gameBall } from './script.js'
+ 
+let rotate = 0
+export let bottom = 0
+let gravity = 0.9
+let isjumping = false
+
+export const rotateBall = () => {
+   setInterval(() => {
+      rotate++
+
+      gameBall.style.transform = 'rotate(' + rotate + 'deg)'
+
+      if (rotate === 360) {
+         rotate = 0
+      }
+   }, 1000/60)
+
+   window.addEventListener('keydown', event => jumpBall(event))
+}
+
+const jumpBall = (event) => {
+   if (isjumping) return
+
+   if (event.key === 'ArrowUp' ){ 
+
+    let upJump =  setInterval(() => {
+         if (bottom > 130) {
+            clearInterval(upJump)
+            
+            let down = setInterval(() => {
+
+               if (bottom < 6) {
+                  clearInterval(down)
+
+                  isjumping = false
+               }
+             
+               bottom -= 5
+               
+               gameBall.style.bottom = `${bottom}px` 
+               
+            }, 1000/60)
+         }
+
+         isjumping = true
+
+         bottom += 15
+         bottom = bottom * gravity
+
+         gameBall.style.bottom = `${bottom}px` 
+
+      }, 1000/60)
+   }
+}
+
---
+++ script.js
@@ -0,0 +1,55 @@
+import { rotateBall } from './ball.js'
+import { createBox,scores } from './box.js'
+
+const main = document.querySelector('.main')
+
+export const renderGame = () => {
+    const mainText = document.createElement('div')
+    mainText.classList.add('main__text')
+    mainText.textContent = 'Press Any Key To Start'
+    main.appendChild(mainText)
+    
+    const mainGame = document.createElement('div')
+    mainGame.classList.add('main__game')
+    main.appendChild(mainGame)
+
+    const game = document.createElement('div')
+    game.classList.add('game')
+    mainGame.appendChild(game)
+
+    const gameElement = document.createElement('div')
+    gameElement.classList.add('game-item')
+    game.appendChild(gameElement)
+
+    const gameBall = document.createElement('div')
+    gameBall.classList.add('game__ball')
+    gameBall.textContent = 'K'
+    gameElement.appendChild(gameBall)
+
+    const ground = document.createElement('div')
+    ground.classList.add('game__ground')
+    game.appendChild(ground)
+
+    const score = document.createElement('div')
+    score.classList.add('score')
+    score.textContent = `Score: ${scores}`
+    ground.appendChild(score)
+}
+
+renderGame()
+
+export const gameBall = document.querySelector('.game__ball')
+
+const mainText = document.querySelector('.main__text')
+
+const startGame = () => {
+    mainText.style.visibility = 'hidden'
+
+    window.removeEventListener('keypress', startGame)
+
+    rotateBall()
+
+    createBox()
+}
+
+window.addEventListener('keypress', startGame)
